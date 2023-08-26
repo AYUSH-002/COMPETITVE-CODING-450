62a5767daeced7fde279ff11ccd2c71101be5bc3
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-08-26 21:24:38 +0530

    Add files via upload

---
+++ commonRowIndex.cpp
@@ -0,0 +1,65 @@
+// https://practice.geeksforgeeks.org/problems/find-duplicate-rows-in-a-binary-matrix/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{   
+public:
+vector<int> repeatedRows(vector<vector<int>> matrix, int M, int N) 
+{ 
+    map<vector<int>,int>umap;
+    vector<int>ans;
+    for(int i=0;i<M;i++)
+    {
+        if(umap.find(matrix[i])!=umap.end())
+        {
+            ans.push_back(i);
+        }
+        else
+        {
+            umap[matrix[i]]++;
+        }
+    }
+    return ans;
+} 
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        vector<int> ans = ob.repeatedRows(matrix, row, col);
+
+
+        for (int i = 0; i < ans.size(); ++i)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
